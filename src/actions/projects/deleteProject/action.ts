4f3a4fd5375d46104dfd1653d075ff17cf7e042a
Jason Petro <jasonpetro865@gmail.com>
2025-09-17 01:26:34 -0600

    Migration to iron session and refactored next.js server actions (#14)
    * added env and constants file
    
    * fully migrated to iron session
    
    * updated packages
    
    * resolve code rabbit reviews

--- src/actions/projects/deleteProject/action.ts
+++ src/actions/projects/deleteProject/action.ts
@@ -1,15 +1,16 @@
-"use server";
+'use server';
 
-import { authActionClient } from "@/lib/action";
-import { deleteProject } from "./logic";
-import { deleteProjectSchema } from "./schema";
+import { authActionClient } from '@/lib/action';
+import { deleteProject } from './logic';
+import { deleteProjectSchema } from './schema';
 
 export const deleteProjectAction = authActionClient
   .inputSchema(deleteProjectSchema)
+  .metadata({ actionName: 'deleteProject' })
   .action(async ({ parsedInput, ctx }) => {
     try {
-      return await deleteProject(parsedInput, ctx.user.userId);
+      return await deleteProject(parsedInput, ctx.user.id);
     } catch (error) {
-      throw new Error("Something went wrong", { cause: error });
+      throw new Error('Something went wrong', { cause: error });
     }
   });
