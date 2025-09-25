822e615a67e8e2871299e08b42f790dae9ad4e37
Max Enfayz <ashenarmor558@gmail.com>
2025-09-25 04:28:52 -0600

    redirect to login and pass userid for get projects

--- src/actions/projects/getProjects.ts
+++ src/actions/projects/getProjects.ts
@@ -2,7 +2,10 @@ import { Project } from '@/generated/prisma';
 import { prisma } from '@/lib/prisma';
 import { Result, success } from '@/lib/result';
 
-export async function getProjects(): Promise<Result<Project[]>> {
-  const projects = await prisma.project.findMany();
+export async function getProjects(userId: string): Promise<Result<Project[]>> {
+  const projects = await prisma.project.findMany({
+    where: { userId },
+    orderBy: { createdAt: 'desc' }
+  });
   return success(projects);
 }
