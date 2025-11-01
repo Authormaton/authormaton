716ba9e5428bc7223a9d23b45ce36af8f73ef108
Erik Arne <erik.arne.eth@gmail.com>
2025-11-01 13:23:07 +0000

    feat: Add immediate flag and improve typings to useDebounce hook

--- src/hooks/use-debounce.ts
+++ src/hooks/use-debounce.ts
@@ -1,15 +1,41 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 
-export function useDebounce<T>(value: T, delay?: number): T {
+/**
+ * `useDebounce` is a custom React hook that debounces a value.
+ *
+ * @template T The type of the value being debounced.
+ * @param {T} value The value to debounce.
+ * @param {number} [delay=500] The delay in milliseconds before the debounced value updates. Defaults to 500ms.
+ * @param {boolean} [immediate=false] If true, the function is called immediately, then debounced. Defaults to false.
+ * @returns {T} The debounced value.
+ *
+ * @example
+ * // Debounce a search input value with a 500ms delay
+ * const debouncedSearchTerm = useDebounce(searchTerm, 500);
+ *
+ * @example
+ * // Debounce a click handler, firing immediately and then preventing further calls for 1000ms
+ * const handleClick = useDebounce(() => console.log('Clicked!'), 1000, true);
+ */
+export function useDebounce<T>(value: T, delay: number = 500, immediate: boolean = false): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
+  const initialCall = useRef(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    if (immediate && initialCall.current) {
+      setDebouncedValue(value);
+      initialCall.current = false;
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [value, delay]);
+  }, [value, delay, immediate]);
 
   return debouncedValue;
 }
