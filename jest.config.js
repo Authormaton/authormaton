717c0f1218d13813f1ddd4d9d690a18753f131a7
Jason Petro <jasonpetro865@gmail.com>
2025-11-14 09:21:25 +0000

    Add unit tests for useMobile hook

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,9 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
-    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|ts|tsx)$': ['babel-jest', {
+      presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript', '@babel/preset-react'],
+    }],
   },
   transformIgnorePatterns: [
     '/node_modules/(?!( @radix-ui|lucide-react|react-icons|jest-environment-jsdom)).+'
